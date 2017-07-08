5507c477390c5af0b78ff3db4438eb7f9898102d
yoskeoka <yoske95@gmail.com>
2017-07-08 17:04:23 +0900

    Declaring a Winner

--- src/index.tsx
+++ src/index.tsx
@@ -23,8 +23,10 @@ interface BoardState {
 class Board extends React.Component<{}, BoardState> {
     constructor() {
         super();
-        const sqrs = Array<string>(9);
-        sqrs.forEach((_, i, array) => array[i] = null);
+        const sqrs = [] as string[];
+        for (let i = 0; i < 9; i++) {
+            sqrs[i] = null;
+        }
         this.state = {
             squares: sqrs,
             xIsNext: true,
@@ -33,6 +35,9 @@ class Board extends React.Component<{}, BoardState> {
 
     handleClick(i: number) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? "X" : "O";
         this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
     }
@@ -45,7 +50,18 @@ class Board extends React.Component<{}, BoardState> {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+        const winner = calculateWinner(this.state.squares);
+        let status: string;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            if (this.state.squares.every((s) => s != null)) {
+                status = "Draw game";
+            } else {
+                status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+            }
+
+        }
 
         return (
             <div>
@@ -92,3 +108,26 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+// ========================================
+
+function calculateWinner(squares: string[]): string {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
